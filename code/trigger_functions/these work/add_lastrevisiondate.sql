88de95ad5eed44be7be13dc29e207968b8778386
Nart Tamash <nart-t@users.noreply.github.com>
2019-01-20 17:10:07 +0000

    Update add_lastrevisiondate.sql

--- code/trigger_functions/these work/add_lastrevisiondate.sql
+++ code/trigger_functions/these work/add_lastrevisiondate.sql
@@ -1,4 +1,9 @@
-﻿-- Function: public.add_last_revision_date()
+-- Loader. Copyright (c) Nart Tamash.
+-- Licensed under [GNU GPLv3](https://bit.ly/2HGhaNl).
+
+
+
+-- Function: public.add_last_revision_date()
 
 -- DROP FUNCTION public.add_last_revision_date();
 
